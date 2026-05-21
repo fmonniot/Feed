69a3f0a3e081bfa1207a210ac2dbc9dc1931b638
François Monniot <francoismonniot@gmail.com>
2026-05-21 07:24:39 -0700

    Android debug build has a different application id

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,9 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
